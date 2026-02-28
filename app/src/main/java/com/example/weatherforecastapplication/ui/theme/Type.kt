976ffd497f4ed93b0c7cf4df052fe6c7288bf3db
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2026-02-28 21:08:53 +0200

    updated themes and type and colors

--- app/src/main/java/com/example/weatherforecastapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/weatherforecastapplication/ui/theme/Type.kt
@@ -2,33 +2,36 @@ package com.example.weatherforecastapplication.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.weatherforecastapplication.R
 
-// Set of Material typography styles to start with
+val CartoonFontFamily = FontFamily(
+    Font(R.font.baloo_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = CartoonFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 64.sp, // Big temperature text
+        lineHeight = 72.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleLarge = TextStyle(
+        fontFamily = CartoonFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp, // Screen headers
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = CartoonFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp, // Main reading text
+    ),
+    labelMedium = TextStyle(
+        fontFamily = CartoonFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp, // Bottom Nav text
     )
-    */
 )
